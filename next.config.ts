f44a505d409cdcc7a6f6924b544e2d9b6ad147ad
Sachin .R <rsachinsachi@gmail.com>
2025-12-23 20:16:27 +0000

    update UI component

--- next.config.ts
+++ next.config.ts
@@ -11,24 +11,7 @@ const nextConfig: NextConfig = {
   },
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
+
     ],
   },
 };
